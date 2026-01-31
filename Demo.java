8662b2476c38a9332e65ef6a2b28d12418ce457d
vsirisha115-source <vsirisha115@gmail.com>
2026-01-31 14:32:05 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo{
+    public static void main(String[] args){
+        int a=10;
+        System.out.println("Hello world");
+        System.out.print(a);
+        System.out.print("hello");
+        a=20;
+        System.out.println(a);
+        System.out.print(a);
+    
+    }
+}
