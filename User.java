91528db335ca81c9da63adccb99090607f46400a
vsirisha115-source <vsirisha115@gmail.com>
2026-01-31 15:04:22 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class User{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Age");
+        int age=sc.nextInt();
+        System.out.println( "Age  of the use is:"+age);
+    }
+}
