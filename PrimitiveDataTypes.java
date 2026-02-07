e81b43f61f8ee933b0ac20d43b74811c3ad3d1bf
vsirisha115-source <vsirisha115@gmail.com>
2026-02-07 11:05:39 +0530

    Add files via upload

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,19 @@
+public class PrimitiveDataTypes{
+    public static void main(String[] args){
+        byte b=127;
+        short c=128;
+        int e=345678654;
+        double d=34564.4565455365;
+        long l=5763453794247645976l;
+        char ch='A';
+        char B=69;
+        boolean r=true;
+        System.out.println("byte:"+b);
+        System.out.println("Short:"+c);
+        System.out.println("double:"+d);
+        System.out.println("long:"+l);
+        System.out.println("char:"+ch);
+        System.out.println("char with:"+B);
+        System.out.println("boolean:"+r);
+    }
+}
