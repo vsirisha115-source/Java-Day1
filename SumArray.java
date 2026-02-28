a33d79d52166a4df03a6bd404f322a9ae4c26bd6
vsirisha115-source <vsirisha115@gmail.com>
2026-02-28 14:37:31 +0530

    Add files via upload

---
+++ SumArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class SumArray{
+    public static void main(String[] args){
+        int[] arr={10,20,30,40};
+int sum=0;
+for( int i=0;i<arr.length;i++){
+    sum+=arr[i];
+}
+System.out.println("the sm is:"+sum);
+
+    }
+}
