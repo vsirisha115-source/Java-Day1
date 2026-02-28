d93e29355a505b598ce0f7cbf061dceaab73e1df
vsirisha115-source <vsirisha115@gmail.com>
2026-02-28 14:22:46 +0530

    Add files via upload

---
+++ RightTriangle.java
@@ -0,0 +1,13 @@
+public class RightTriangle{
+    public static void main(String[] args){
+        int i,j;
+        int n=5;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+                System.out.print("*"+"");
+            }
+            System.out.println("");
+        }
+        
+    }
+}
